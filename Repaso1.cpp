4747da46f3c4842e5d2fb6b67d28399cc8859541
JoseTorres24 <luis.lopeztr@uanl.edu.mx>
2025-06-02 15:33:07 -0600

    Ejercicios de variables y operadores (Repaso1.c)

--- Repaso1.cpp
+++ Repaso1.cpp
@@ -3,12 +3,8 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
-//Programa para calcular el para calcular minutos y seguntos a partir de segundos
+//Ejercicios de variables y operadores (Repaso1.c)
 int main() {
-	int segundos, minutos, segundosRestantes;
-	printf("Introduce el numero de segundos: ");
-	scanf("%d", &segundos);
-	minutos = segundos / 60; // Calcular minutos
-	segundosRestantes = segundos % 60; // Calcular segundos restantes
-	printf("%d segundos son %d:%d min.\n", segundos, minutos, segundosRestantes);
+	
+
 }

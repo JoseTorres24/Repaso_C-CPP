c785b438d6c267b59df56cfd8a97904fab9069d2
JoseTorres24 <luis.lopeztr@uanl.edu.mx>
2025-06-02 15:01:35 -0600

    Ejercicio para poder visualizar el resuldo de una división entre enteros y reales

--- Repaso1.cpp
+++ Repaso1.cpp
@@ -3,17 +3,28 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
-//Ejercicio para calcular el área de un triángulo rectángulo
+//Ejercicio para poder visualizar el resuldo de una división entre enteros y reales
+// y ver cómo se comporta el resultado dependiendo del tipo de dato utilizado.
+// Se debe tener en cuenta que si se divide un entero entre un entero, el resultado es un entero truncado
+// y si se divide un entero entre un real, el resultado es un real con decimales.
+// Si se divide un real entre un entero, el resultado es un real con decimales.
+// Si se divide un real entre un real, el resultado es un real con decimales.
+// El resultado de la división entre enteros se trunca, mientras que el resultado de la división entre reales se muestra con decimales.
+
 
 int main() {
-	float area, base, altura;
-	printf("Introduce la base del triángulo: ");
-	scanf("%f", &base);
-	printf("\nIntroduce la altura del triángulo: ");
-	scanf("%f", &altura);
-	area = (base * altura) / 2;
-	printf("\nUn triangulo rectángulo de altura %.2f y base %.2f, tiene un área de %.2f",altura,base, area);
-
-
+	int i1, i2, i3;
+	float r1, r2, r3;
+	i1 = 12;
+	i2 = 5;
+	r1 = 12.0;
+	r2 = 5;
+	r3 = r1 / r2;
+	printf("El resultado de la división es: %f\n", r3);
+	r3 = i1 / i2;
+	printf("El resultado de la división es: %f\n", r3);
+	i3 = i1 / r2;
+	printf("El resultado de la división es: %d\n", i3);
+	
 	return 0;
 }

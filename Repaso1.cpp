f4ccde75215ce506bfc36dc9e20735a40c123198
JoseTorres24 <luis.lopeztr@uanl.edu.mx>
2025-06-02 15:20:47 -0600

    Programa para calcular el para calcular minutos y seguntos a partir de segundos

--- Repaso1.cpp
+++ Repaso1.cpp
@@ -1,15 +1,14 @@
 // Repaso1.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 #define _CRT_SECURE_NO_WARNINGS
-#define PI 3.14159265358979323846
 #include <stdio.h>
 
-//Programa para calcular el perimetro de un circunferencia
+//Programa para calcular el para calcular minutos y seguntos a partir de segundos
 int main() {
-	float perimetro = 0.0, radio = 0.0 ;
-	printf("Introduce el radio de la circunferencia (esto es en cm): ");
-	scanf("%f", &radio);
-	perimetro = 2 * PI * radio;
-	printf("El perimetro de la circunferencia es: %.2f cm\n", perimetro);
-	return 0;
+	int segundos, minutos, segundosRestantes;
+	printf("Introduce el numero de segundos: ");
+	scanf("%d", &segundos);
+	minutos = segundos / 60; // Calcular minutos
+	segundosRestantes = segundos % 60; // Calcular segundos restantes
+	printf("%d segundos son %d:%d min.\n", segundos, minutos, segundosRestantes);
 }

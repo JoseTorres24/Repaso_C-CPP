ea11e98038be065faf793e24919cd62796c4deb3
JoseTorres24 <luis.lopeztr@uanl.edu.mx>
2025-06-03 16:15:35 -0600

    Ejercicios de a>b>c sin estructuras de control

--- Repaso1.cpp
+++ Repaso1.cpp
@@ -3,14 +3,20 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
-//Ejercicio del nombre del usuario y saludo
+// valor maximo secuencial entre a, b y c
 int main() {
-	
-	char nombre[30];
-	
-	printf("\nHola bienvenido!!");
-	printf("\nPor favor, introduce tu nombre: ");
-	scanf("%[^\n]", nombre);
-	printf("\nHola %s, vamos a hacer un repaso de C.\n", nombre);
 
+	printf("Valor maximo secuencial entre a, b y c (Verificamos si es verdadero 1 y 0)\n");
+	int a = 0, b = 0, c = 0;
+	int maximo = 0;
+	printf("\nIntroduzca el valor de la variable a: ");
+	scanf("%d", &a);
+	printf("\nIntroduzca el valor de la variable b: ");
+	scanf("%d", &b);
+	printf("\nIntroduzca el valor de la variable c: ");
+	scanf("%d", &c);
+	// Solo es necesario saber si a es mayor b o mayor c
+    maximo = (a > b && a > c && b > c) ? 1 : 0;
+	printf("El valor secuencial de los valores %d, %d y %d es : %d\n", a, b, c, maximo);
+	return 0;
 }

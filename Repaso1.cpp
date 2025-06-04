d575c5055e45cd0f309a4b1704a82d8789e0f0aa
JoseTorres24 <luis.lopeztr@uanl.edu.mx>
2025-06-04 00:00:54 -0600

    Ejercicios de estructuras de control IF (Cantidad de numeros par y suma de digitos en numero de solo dos cifras)

--- Repaso1.cpp
+++ Repaso1.cpp
@@ -4,28 +4,36 @@
 #include <stdio.h>
 #include <math.h>
 
-//Ejercicios de estructuras de control IF (Valor de funcion)
+//Ejercicios de estructuras de control IF (Cantidad de numeros par y suma de digitos en numero de solo dos cifras)
 int main() {
-	float x = 0, y = 0;
-	int resultado = 0;
-
-	printf("\n x<=0 -> y = x + 3\n x>0 -> y = x^2 + 2x\n");
-	printf("\nIngresar el valor de x = ");
-	resultado = scanf("%f", &x);
+	int ResultadoResto = 0;
+	int ResultadoDivisiion = 0;
+	int numero = 0;
+	int resultado = 0; //Lectura
+	int SumaDigitos = 0;
+	int CantidadPares = 0;
+	printf("\nIngresar el numero: ");
+	resultado = scanf("%d", &numero);
+	
 	if (resultado == 0) {
 		printf("\nError de lectura");
 	}
-	else {
-		if (x <= 0) {
-			y = x + 3;
+	else
+	{	//Verificar que el numero sea de dos digitos
+		if (numero > 9 && numero < 99) {
+			ResultadoResto = numero % 10;
+			ResultadoDivisiion = numero / 10;
+			SumaDigitos = ResultadoDivisiion + ResultadoResto;
+			printf("\nLa suma de los digitos del numero %d",SumaDigitos);
+			if (ResultadoResto % 2 == 0) {
+				CantidadPares++;
+			}
+			if (ResultadoDivisiion % 2 == 0) {
+				CantidadPares++;
+			}
+			printf("\nLa cantidad de numeros par que tiene el numero son: %d", CantidadPares);
 		}
-		if (x > 0) {
-			y = pow(x,2) + (2 * x);
-		}
-		printf("\n y = %.2f", y);
+		printf("\nEl numero no es dos cifras");
 	}
-	x = 0;
-	y = 0;
-
 	return 0;
 }

2927a1f73eff932060a4eb4fb4a0fec03eb7c079
JoseTorres24 <luis.lopeztr@uanl.edu.mx>
2025-06-03 23:37:35 -0600

    Ejercicios de estructuras de control IF (Valor de funcion)

--- Repaso1.cpp
+++ Repaso1.cpp
@@ -2,33 +2,30 @@
 //
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#include <math.h>
 
-//Ejercicios de estructuras de control IF (Entradas de cine)
+//Ejercicios de estructuras de control IF (Valor de funcion)
 int main() {
+	float x = 0, y = 0;
+	int resultado = 0;
 
-	float entradaCine = 7;
-	int edadCliente = 0;
-	int resultadoLectura = 0;//Procedimiento sencillo de lectura, solo sirve para repasar, solo sirve si se ingresan primero numeros, luego lo demas ya no importa
-	
-	printf("\nIngresar la edad del cliente: ");
-	 resultadoLectura = scanf("%d", &edadCliente);
-	 if (resultadoLectura == 0) {
-		 printf("No se realizo correctamente la lectura, devolviendo un balor de %d", resultadoLectura);
-	 }
-	 
-
-	 else {
-		 if (edadCliente < 5) {
-			 entradaCine -= (entradaCine * .60);
-		 }
-		 if (edadCliente > 60) {
-			 entradaCine -= (entradaCine * .55);
-		 }
-		 printf("\nEl precio de su entrada de cine es %.2f", entradaCine);
-	 }
-	entradaCine = 0;
-	edadCliente = 0;
-
+	printf("\n x<=0 -> y = x + 3\n x>0 -> y = x^2 + 2x\n");
+	printf("\nIngresar el valor de x = ");
+	resultado = scanf("%f", &x);
+	if (resultado == 0) {
+		printf("\nError de lectura");
+	}
+	else {
+		if (x <= 0) {
+			y = x + 3;
+		}
+		if (x > 0) {
+			y = pow(x,2) + (2 * x);
+		}
+		printf("\n y = %.2f", y);
+	}
+	x = 0;
+	y = 0;
 
 	return 0;
 }

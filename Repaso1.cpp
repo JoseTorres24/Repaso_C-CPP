939a4602ddfa3d5d6379134fa960696e578a2962
JoseTorres24 <luis.lopeztr@uanl.edu.mx>
2025-06-04 18:39:00 -0600

    Ejercicio de calcular el area (Switch)

--- Repaso1.cpp
+++ Repaso1.cpp
@@ -2,42 +2,63 @@
 //
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#include <stdlib.h>
 
 
 
+//Ejercicio de calcular el area (Switch)
 int main() {
+	
+	float baseMayor = 0, baseMenor = 0,Altura = 0;
+	float area = 0;
+	int seleccion = 0;
+	printf("\n\t\t\t\tCALCULO DE AREAS");
+	printf("\n\t\t\t\t===================================");
+	printf("\n\t\t\t\t1.- Calcular area de triangulo");
+	printf("\n\t\t\t\t2.- Calcular area de trapecio");
+	printf("\n\t\t\t\t3.- Calcular area de rectangulo");
+	printf("\n\t\t\t\tSeleccionar opcion: ");
+	scanf("%d", &seleccion);
 
-	char Caracter;
-	
-	printf("\nIngresar un caracter para determinar si es vocal: ");
-	Caracter = getchar();
-	
-	
-	switch (Caracter)
+	switch (seleccion)
 	{
-		case 'a':case 'e':case'o': case'i': case'u':
-		case 'A':case 'E':case'O': case'I': case'U': {
-			printf("\nEs vocal carallo");
+		case 1: { 
+
+			printf("\nIngresar altura: ");
+			scanf("%f", &Altura);
+			printf("\nIngresar la base: ");
+			scanf("%f", &baseMayor);
+
+			area = baseMayor * Altura / 2;
 			break;
 		}
+		case 2: {
+			printf("\nIngresar base menor: ");
+			scanf("%f", &baseMenor);
+			printf("\nIngresar base mayor: ");
+			scanf("%f", &baseMayor);
+			printf("\nIngresar altura: ");
+			scanf("%f", &Altura);
+
+			area = (baseMenor + baseMayor * (Altura)) / 2;
+			break;
+		}
+		case 3: {
+			printf("\nIngresar base: ");
+			scanf("%f", &baseMayor);
+			printf("\nIngresar altura: ");
+			scanf("%f", &Altura);
+
+			area = baseMayor * Altura;
+			
+			break;
+		}
+
 		default: {
-			printf("\nNo es vocal");
 			break;
 		}
-
-
 	}
-	//Con recurso de if / else
-	if	(Caracter == 'a'|| Caracter == 'e'
-		|| Caracter == 'i'|| Caracter == 'o' || Caracter == 'u'
-		|| Caracter == 'A' || Caracter == 'E'|| Caracter == 'I'
-		|| Caracter == 'O' || Caracter == 'U') {
-
-		printf("\nEs vocal carallo otra vez");
-		
-	}
-	else {
-		printf("\nNo es vocal carallo otra vez");
-	}
+	printf("\nEl valor del area de la figura seleccionada: %.2f\n", area);
+	system("pause");
 	return 0;
 }

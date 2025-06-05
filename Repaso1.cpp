d3407bdf2dee8bae49118a5e690012162f44efb8
JoseTorres24 <luis.lopeztr@uanl.edu.mx>
2025-06-04 20:49:04 -0600

    Ejercicios sencillos de for

--- Repaso1.cpp
+++ Repaso1.cpp
@@ -6,9 +6,29 @@
 
 
 
-//Ejercicios de(IF, IF-ELSE & SWITCH)
+//Ejercicios de (Estructuras repetitivas)
+//Sencillos
 int main() {
-	
+	int i = 0;
+	printf("\n\tLista de numeros\n\n");
+	for (i = 1; i <= 100; i++) {
+		printf("\tNumero %d\n\n", i);
+	}
 
+	printf("\n\tLista de numeros\n\n");
+	for (i = 1; i <= 100; i = i + 2) {
+		printf("\t\tNumero %d\n\n", i);
+		if (i == 99) {
+			printf("\t\tNumero %d\n\n", i + 1);
+		}
+	}
+	
+	printf("\n\tLista de numeros\n\n");
+	for (i = 100; i >= 1; i --) {
+		printf("\t\tNumero %d\n\n", i);
+
+	}
+	
+	system("pause");
 	return 0;
 }

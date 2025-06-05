e7af354065890c79a99a830858e36aeb7b4ec7e9
JoseTorres24 <luis.lopeztr@uanl.edu.mx>
2025-06-04 19:40:48 -0600

    Ejercicios de(IF, IF-ELSE & SWITCH)

--- Repaso1.cpp
+++ Repaso1.cpp
@@ -6,35 +6,9 @@
 
 
 
-//Ejercicio de calcular la paga de un trabajador (IF, IF-ELSE)
+//Ejercicios de(IF, IF-ELSE & SWITCH)
 int main() {
 	
-	float horasTrabajadas = 0;
-	float horaPagada = 0;
-	float paga = 0;
 
-	printf("\nIngresar lo que se pagara por hora: ");
-	scanf("%f", &horaPagada);
-	printf("\nIngresar cantidad de horas trabajadas: ");
-	scanf("%f", &horasTrabajadas);
-
-	if(horasTrabajadas <= 40 || horasTrabajadas > 0){
-		paga = horasTrabajadas * horaPagada;
-	}
-	else {
-		if (horasTrabajadas > 40 || horasTrabajadas < 50)
-		{
-			paga = horasTrabajadas* (horaPagada*(1.5));
-		}
-		if (horasTrabajadas >= 50) {
-			paga = horasTrabajadas * (horaPagada * (2.0));
-
-		}
-	}
-
-	printf("\nLa paga del trabajador es: %.2f\n", paga);
-
-
-	system("pause");
 	return 0;
 }

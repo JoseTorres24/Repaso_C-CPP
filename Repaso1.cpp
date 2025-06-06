e645fa0197d07b0b440384b6499b13619bb49fdb
JoseTorres24 <luis.lopeztr@uanl.edu.mx>
2025-06-06 00:14:24 -0600

    Ejercicios de arreglos con estructuras repetitivas V1

--- Repaso1.cpp
+++ Repaso1.cpp
@@ -4,101 +4,59 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#define MAX_ARRAY 10
 
-
-
-//Ejercicios de Potencias x^y (WHILE Y VALIDACION)
+//Ejercicios de arreglos con estructuras repetitivas
 //Sencillos
 
 int main() {
+	
+	int num[10] = {0};
+	int sumaNums = 0;
+	float prom = 0;
+	int menor = 0;
+	int mayor = 0;
+	int i = 0;
+	int j = 0;
+	int aux = 0;
 
-	int x = 0, y = 0;
-	int resultado = 1;
-	int continuar = 1;
-	int metodos = 0;
-
-
-	while (continuar != 0) {
-		while (x == 0 && y == 0)
-		{
-			//Metodos sencillos para validar enteros porque la verdad no me se otro
-
-			printf("\nx = ");
-			while (scanf("%d", &x) != 1) {
-				while (getchar() != '\n');
-				system("cls");
-				printf("\nx = ");
-			}
-
-			printf("\ny = ");
-			while (scanf("%d", &y) != 1) {
-				while (getchar() != '\n');
-				system("cls");
-				printf("\ny = ");
-			}
-
-			if (x == 0 && y == 0) {
-				printf("\nNo existe un valor correcto\n");
-				system("pause");
-				system("cls");
-			}
+	for (i = 0; i < 10; i++) {
+		printf("\nIngresar numero %d: ",i + 1);
+		while (scanf("%d", &num[i]) != 1) { 
+			while (getchar() != '\n'); 
+			system("cls");
+			printf("\nIngresar numero: ");
 		}
 		system("cls");
-		
-
-		printf("Metodos para hacer x^y \n\n");
-		printf("\n1.- Con la funcion pow");
-		printf("\n2.- Sin la funcion pow");
-		printf("\nSeleccione opcion: ");
-		while (scanf("%d", &metodos) != 1)
-		{
-			while (getchar() != '\n');
-			system("cls");
-			printf("Metodos para hacer x^y \n\n");
-			printf("\n1.- Con la funcion pow");
-			printf("\n2.- Sin la funcion pow");
-			printf("\nSeleccione opcion: ");
-		}
-
-
-		switch (metodos)
-		{
-			case 1: {
-				resultado = pow(x, y);
-				break;
-			}
-			case 2: {
-
-				int i = 0;
-				if (y == 0) {
-					resultado = 1;
-				}
-				for (i = 0; i < y; i++) {
-					resultado = resultado * x;
-					printf("\nx^%d = %d\n",i + 1,resultado);
-					system("pause");
-				}
-				
-
-				break;
-			}
-			default:
-				printf("\nSeleecione una opcion valida");
-				system("pause");
-				system("cls");
-				break;
-		}
-		system("cls");
-		printf("\nEl resultado es %d", resultado);
-		printf("\nDesea continuar (1 -> Si) (0 -> No): ");
-		while (scanf("%d", &continuar) != 1) {
-			while (getchar() != '\n');
-			system("cls");
-			printf("\nDesea continuar (1 -> Si) (0 -> No): ");
-		}
-		resultado = 1;
-		 
 	}
+	system("cls");
+
+	for (i = 0; i < 10; i++) {
+		sumaNums += num[i];
+	}
+
+	prom = sumaNums / (10.0f);
+	//Ordenamiento de valores de menor a mayor (metodo de la burbuja)
+	for (i = 0; i < 10; i++) {
+		for (j = 0; j < i; j++) {
+			if (num[i] > num[j]) {
+				aux = num[i];
+				num[i] = num[j];
+				num[j] = aux;
+			}
+		}
+	}
+	//Mostrar valores
+	printf("\n\t\t\t\t\tImpresion de valores\n");
+	printf("\n\t\t\t=======================================\n");
+	printf("\n\t\t\tLa suma de todos los numeros: %d", sumaNums);
+	printf("\n\t\t\tEl promedio de los numeros  : %.2f", prom);
+	printf("\n\t\t\tEl numero mayor del arreglo : %d", num[0]);
+	printf("\n\t\t\tEl numero menor del arreglo : %d", num[9]);
+	printf("\n\n\n");
+
+
+
 
 	system("pause");
 	system("cls");

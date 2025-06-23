5a8518f05ba67ec78d86e6ddab2ced32dbf98c7b
JoseTorres24 <luis.lopeztr@uanl.edu.mx>
2025-06-22 19:44:08 -0600

    Ajustando a Ubuntu

--- Repaso1.cpp
+++ Repaso1.cpp
@@ -1,148 +1,128 @@
-#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <conio.h>
-//Para los idiomas
-#include <locale.h>
-#include <windows.h>
+#include <ctype.h>
+#include <ncurses.h>
 
-struct dniLetra {
+#ifdef _WIN32
+#define CLEAR "cls"
+#define PAUSE "pause"
+#include <conio.h>
+#define get_char _getch
+#else
+#define CLEAR "clear"
+#define PAUSE "read -p 'Presione Enter para continuar...' var"
+#include <termios.h>
+#include <unistd.h>
+
+// FunciÃ³n para leer un solo carÃ¡cter (similar a getch)
+char get_char()
+{
+    struct termios oldt, newt;
+    char ch;
+    tcgetattr(STDIN_FILENO, &oldt);
+    newt = oldt;
+    newt.c_lflag &= ~(ICANON | ECHO);
+    tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+    ch = getchar();
+    tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+    return ch;
+}
+#endif
+
+struct dniLetra
+{
     int num;
     char letra;
 };
-
-
-
-void idiomaEspañol();
-
-//Ejercicio de DNI y Letra 
 
 int dniCorrecto(int);
 char dniCaracterCorrespondiente(int);
 
 int main()
 {
-    idiomaEspañol();
-    char c = NULL;
-    int intentos = 0;
     int dniNum = 0;
-    char caracterDni = NULL;
-    
-    while (dniCorrecto(dniNum) != 1){
+    char caracterDni = '\0';
+    char c = '\0';
+
+    while (dniCorrecto(dniNum) != 1)
+    {
         printf("\nIngresar DNI por favor: ");
-        while (scanf("%d", &dniNum) != 1) {
-
-            while ((c = getchar()) != EOF && c != '\n');
-            system("cls");
+        while (scanf("%d", &dniNum) != 1)
+        {
+            while ((c = get_char()) != EOF && c != '\n')
+                ;
+            system(CLEAR);
             printf("\nIngresar DNI por favor: ");
-           
         }
 
-        if (dniCorrecto(dniNum) == 0) {
+        if (dniCorrecto(dniNum) == 0)
+        {
             printf("\nIngresar un nuevo valor, por favor!\n");
-            system("pause");
-            system("cls");
+            system(PAUSE);
+            system(CLEAR);
         }
+    }
 
-    } 
     char cepo = dniCaracterCorrespondiente(dniNum);
     do
     {
-        printf("\nIngresar el caracter correpodiente: ");
-        caracterDni = _getch();
-        if (!isalpha(caracterDni)) {
+        printf("\nIngresar el caracter correspondiente: ");
+        caracterDni = get_char();
+        printf("%c\n", caracterDni); // Mostrar lo que se ingresÃ³
+
+        if (!isalpha(caracterDni))
+        {
             printf("\nEl caracter ingresado no parece ser del alfabeto\n");
-            system("pause");
-            system("cls");
+            system(PAUSE);
+            system(CLEAR);
         }
-     
+
     } while (!isalpha(caracterDni));
-       
+
     caracterDni = toupper(caracterDni);
-    printf("\n%c\n", caracterDni);
+    printf("\nLetra ingresada: %c\n", caracterDni);
 
-    
-
-    if (cepo != caracterDni) {
-        printf("\nEl caracter no es coincidente con el DNI\n El NIF no es correcto\n");
-
+    if (cepo != caracterDni)
+    {
+        printf("\nEl caracter no es coincidente con el DNI\nEl NIF no es correcto\n");
     }
-    else {
-        printf("\nEl caracter es coincidente con el DNI\n El NIF es correcto\n");
+    else
+    {
+        printf("\nEl caracter es coincidente con el DNI\nEl NIF es correcto\n");
     }
 
-
+    return 0;
 }
 
-int dniCorrecto(int dni) {
-
+int dniCorrecto(int dni)
+{
     int contador = 0;
-    int resultado = 0;
+    int resultado = dni;
 
-    resultado = dni;
-
-    while (resultado > 0) {
-        resultado/= 10;
+    while (resultado > 0)
+    {
+        resultado /= 10;
         contador++;
     }
 
-    if (contador == 8) {
-        return 1;
-    }
-
-    return 0; 
+    return contador == 8;
 }
-char dniCaracterCorrespondiente(int dni) {
 
-    dniLetra resto[23] = {
-        {0,'T'},
-        {1,'R' },
-        {2,'W'},
-        {3,'A'},
-        {4,'G'},
-        {5,'M'},
-        {6,'Y'},
-        {7,'F'},
-        {8,'P'},
-        {9,'D'},
-        {10,'X'},
-        {11,'B'},
-        {12,'N'},
-        {13,'J'},
-        {14,'Z'},
-        {15,'S'},
-        {16,'Q'},
-        {17,'V'},
-        {18,'H'},
-        {19,'L'},
-        {20,'C'},
-        {21,'K'},
-        {22,'E'}
-    };
-    
-    char c = NULL;
-    int resultado = dni;
-    int i = 0;
-    resultado %= 23;
+char dniCaracterCorrespondiente(int dni)
+{
+    struct dniLetra resto[23] = {
+        {0, 'T'}, {1, 'R'}, {2, 'W'}, {3, 'A'}, {4, 'G'}, {5, 'M'}, {6, 'Y'}, {7, 'F'}, {8, 'P'}, {9, 'D'}, {10, 'X'}, {11, 'B'}, {12, 'N'}, {13, 'J'}, {14, 'Z'}, {15, 'S'}, {16, 'Q'}, {17, 'V'}, {18, 'H'}, {19, 'L'}, {20, 'C'}, {21, 'K'}, {22, 'E'}};
 
+    int resultado = dni % 23;
 
-    while (resto[i].num != resultado) {
-        for (i = 0; i < 23 ; i++) {
-            if (resto[i].num == resultado) {
-                c = resto[i].letra;
-                break;
-            }
+    for (int i = 0; i < 23; i++)
+    {
+        if (resto[i].num == resultado)
+        {
+            return resto[i].letra;
         }
     }
-    printf("\n%c\n",c);
 
-    return c;
-}
-
-void idiomaEspañol() {
-    setlocale(LC_ALL, "spanish"); // Cambiar locale - Suficiente para máquinas Linux
-    SetConsoleCP(1252); // Cambiar STDIN -  Para máquinas Windows
-    SetConsoleOutputCP(1252);
-
+    return '?'; // En caso de error
 }
